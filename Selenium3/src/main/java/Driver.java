5a80fdf54b76890151644c00aaa157a34260b813
Hemay12 <hemaysorathia1@gmail.com>
2024-08-15 01:41:24 +0530

    Search in google and validate result

--- Selenium3/src/main/java/Driver.java
+++ Selenium3/src/main/java/Driver.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
 public class Driver {
 
@@ -7,7 +10,28 @@ public class Driver {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath(("//textarea[@id='APjFqb']"))).sendKeys("Cars");
+		
+		driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']  //input[@class='gNO89b']")).click();
+		
+		boolean searchResult = false;
+		try {
+			WebElement exist = driver.findElement(By.xpath("//a[@aria-label='Page 2']"));
+			searchResult = true;
+			
+		} catch (NoSuchElementException e){
+			searchResult = false;
+		}
+		
+		if(searchResult) {
+			System.out.println("Search element found");
+		}else {
+			System.out.println("Search element not found");
+		}
+
 
 	}
 
-}
+} 
