b4adb5163832ceec2098579255b4680a39c474a0
Hemay12 <hemaysorathia1@gmail.com>
2024-09-14 18:36:57 +0530

    Sorting of characters

---
+++ Selenium3/src/main/java/Intermediate/SortCharacter.java
@@ -0,0 +1,41 @@
+package Intermediate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SortCharacter {
+
+	public static void main(String[] args) {
+			WebDriver driver  = new ChromeDriver();
+			driver.get("https://softwaretestingpro.github.io/Automation/Intermediate/I-2.23-SortCharacters.html");
+			
+			List<WebElement> list = driver.findElements(By.xpath("//div[@class='letter-box']"));
+			int a = list.size();
+			System.out.println(a);
+			char arr[] = new char[a];
+	 		for(int i= 0; i<a;i++) {
+				WebElement x =list.get(i);
+				String str = x.getText();
+				char j = str.charAt(0);
+				arr[i]=j;
+				System.out.print(arr[i]);
+			}
+	 		
+	 		
+	 		Arrays.sort(arr);
+	 		System.out.println(Arrays.toString(arr));
+	 		
+	 		List<WebElement> list1 = driver.findElements(By.xpath("//input[@type='text']"));
+	 		for(int i =0; i<a;i++) {
+	 			WebElement y = list1.get(i);
+	 			y.sendKeys(String.valueOf(arr[i]));
+	 		}
+
+		}
+
+	}
